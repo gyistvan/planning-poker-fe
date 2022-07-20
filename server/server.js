042b913acf1516352307c04bb260fed39a30fa7c
Istvan Gyarmati <istvan_gyarmati@epam.com>
2022-07-20 10:57:23 +0200

    -

--- server/server.js
+++ server/server.js
@@ -4,9 +4,11 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3001;
 app.use(express.static(publicPath));
+
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
+
 app.listen(port, () => {
    console.log('Server is up!');
 });
